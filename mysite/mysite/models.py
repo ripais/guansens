875eed21731019b2b8e4d21b16a62ddd70d2bd31
ripais <ricardo@guanchip.com>
2017-03-17 16:37:49 +0000

    F.L.O.S

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 
 class RaspTable(models.Model):
-    id_rasp = models.IntegerField(primary_key=True)
+    id_rasp = models.AutoField(primary_key=True)
     numsensor = models.IntegerField(u'Numero sensores')
     localaddress = models.CharField(u'LAN IP', max_length=15)
     connectionhost = models.CharField(u'WAN IP', max_length=15)
@@ -21,14 +21,14 @@ class RaspTable(models.Model):
 
 
 class UsersRasp(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    rasp = models.ForeignKey(RaspTable, on_delete=models.CASCADE, default=None)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    id_rasp = models.ForeignKey(RaspTable, on_delete=models.CASCADE, default=None)
 
     def __unicode__(self):
         return '%s - %s' % (self.user.username, self.rasp)
 
 class Event(models.Model):
-    id_event = models.IntegerField(primary_key=True)
+    id_event = models.AutoField(primary_key=True)
     name = models.CharField(u'Name', max_length=100)
     description = models.CharField(u'Description', max_length=255)
     id_user = models.ForeignKey(UsersRasp, on_delete=models.CASCADE,default=None)
@@ -40,8 +40,8 @@ class Event(models.Model):
         return '%s - %s' % (self.name, self.time)
 
 class SensorTable(models.Model):
-    id_sensor = models.IntegerField(primary_key=True)
-    rasp = models.ForeignKey(RaspTable, on_delete=models.CASCADE, default=None)
+    id_sensor = models.AutoField(primary_key=True)
+    id_rasp= models.ForeignKey(RaspTable, on_delete=models.CASCADE, default=None)
     msensor = models.CharField(max_length=8)
     located = models.CharField(max_length=32)
     address = models.CharField(max_length=15)
@@ -57,7 +57,7 @@ class SensorTable(models.Model):
 
 
 class DataTable(models.Model):
-    sensor = models.ForeignKey(SensorTable, on_delete=models.CASCADE, default=None)
+    id_datatable = models.AutoField(primary_key=True)
     value = models.FloatField()
     fecha = models.FloatField(blank=True, null=True)
     actual = models.DateTimeField(blank=True, null=True)
@@ -65,6 +65,7 @@ class DataTable(models.Model):
     numsensor = models.IntegerField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, blank=True)
     updated = models.DateTimeField(auto_now=True, blank=True)
+    sensor  = models.ForeignKey(SensorTable, on_delete=models.CASCADE, default=None)
 
     def __unicode__(self):
         return '%d %s' % (self.value, self.fecha)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -92,10 +92,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'wifly': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wifly',
         'USER': 'wifly',
